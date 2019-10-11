a858266a0f5839d23cc008e8385e3f98c729fb62
Eric <eric@192.168.0.105>
2019-10-11 14:25:58 +0800

    实现了快排partition的另外一个版本
    Signed-off-by: Eric <eric@192.168.0.105>

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,4 +1,5 @@
 
+
 class Solution:
 
     def __init__(self):
@@ -27,6 +28,39 @@ class Solution:
         return self.q_sort(nums, 0, len(nums) - 1)
 
 
+# nums = [3, 2, 1, 5, 6, 4]
+# result = Solution().quick_sort(nums)
+# print(result)
+
+def swap(arr, m, n):
+    arr[m], arr[n] = arr[n], arr[m]
+
+# 用迭代的方式实现一个版本
+
+def partition(arr, low, high):
+    temp = arr[high]
+    i = low - 1
+    for j in range(low, high-1):
+        if arr[j] < temp:
+            i += 1
+            swap(arr, i, j)
+    swap(arr, i+1, high)
+    return i+1
+
+
+def q_sort(arr: list, start: int, end: int):
+    if start < end:
+        pivot = partition(arr, start, end)
+        print(pivot)
+        q_sort(arr, start, pivot - 1)
+        q_sort(arr, pivot + 1, end)
+    return arr
+
+
+def quick_sort(arr):
+    return q_sort(arr, 0, len(arr) - 1)
+
+
 nums = [3, 2, 1, 5, 6, 4]
-result = Solution().quick_sort(nums)
+result = quick_sort(nums)
 print(result)
