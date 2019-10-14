3715527fc0e6d817bf74d8316ce2af730df696d8
Erice <forever_1947@163.com>
2019-10-14 21:05:39 +0800

    update merge sort
    Signed-off-by: Erice <forever_1947@163.com>

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -1,34 +1,35 @@
 
-def swap(arr, m, n):
-    arr[m], arr[n] = arr[n], arr[m]
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
 
-# 用迭代的方式实现一个版本
+    length = len(nums)
 
-def partition(arr, low, high):
-    print(arr, low, high)
-    temp = arr[high]
-    i = low - 1
-    for j in range(low, high-1):
-        if arr[j] < temp:
-            i += 1
-            swap(arr, i, j)
-    if arr[i+1] > temp:
-        swap(arr, i+1, high)
-    return i+1
+    mid = length // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+    return merge(left, right)
 
 
-def q_sort(arr: list, start: int, end: int):
-    if start < end:
-        pivot = partition(arr, start, end)
-        q_sort(arr, start, pivot - 1)
-        q_sort(arr, pivot + 1, end)
-    return arr
+def merge(l, r):
+    merged_list = []
+    j, k = 0, 0
+    while j < len(l) and k < len(r):
+        if l[j] < r[k]:
+            merged_list.append(l[j])
+            j += 1
+        else:
+            merged_list.append(r[k])
+            k += 1
+    if len(l) == j:
+        for i in r[k:]:
+            merged_list.append(i)
+    else:
+        for i in l[j:]:
+            merged_list.append(i)
+    return merged_list
 
 
-def quick_sort(arr):
-    return q_sort(arr, 0, len(arr) - 1)
-
-
-nums = [3, 2, 1, 5, 6, 4]
-result = quick_sort(nums)
+nums = [3, 2, 8, 1, 7, 5, 3, 9, 6, 4]
+result = merge_sort(nums)
 print(result)
